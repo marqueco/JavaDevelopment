7e2b2c3f765d989e468406f5c9db795d70100dca
Maria Arquer <Maria Arquer@192.168.1.136>
2019-07-17 22:15:46 +0200

    Copies seguretat 17/7

---
+++ Exercicis/src/pla5/Person.java
@@ -0,0 +1,88 @@
+package pla5;
+import java.util.Scanner;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	private int height;
+	private int weight;
+	private String occupation;
+	private String education;
+	private int contribution;
+	
+	public Person(String name) {
+		this.name = name;
+		this.age = 0;
+		this.weight = 0;
+		this.height = 0;
+		this.occupation = "";
+		this.education = ""; 
+		this.contribution = 0;
+		
+		
+	}
+	
+	public void printPerson() {
+		System.out.println(this.name + " is " + this.age + " years old.");
+	}
+	
+	public void becomeOlder() {
+		this.age++;
+	}
+	
+	public boolean isAdult() {
+		if (this.age < 18) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean isActive() {
+		if (this.occupation == "") {
+			return false;
+		}
+		return true;
+	}
+	
+	public double bodyMassIndex() {
+		double heightInMeters = this.height * 1.0;
+		double weightInKg = this.weight * 1.0;
+		
+		return weightInKg / Math.pow(heightInMeters, 2);
+	}
+
+	public String toString() {
+	
+		return "Information under entry " + this.name + ": age(" + this.age + "); BMI(" + bodyMassIndex();
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getContribution() {
+		return this.contribution;
+	}
+	
+	public String getEducation() {
+		return this.education;
+	}
+	public void setWeight() {
+		Scanner reader = new Scanner(System.in);
+		this.weight = Integer.parseInt(reader.nextLine());
+	}
+	
+	public void setHeight() {
+		this.height = height;
+	}
+	
+}
