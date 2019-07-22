6e03dd6aeb7785e76aaf2a5e6149c89d07b91f2c
mati <mati@DESKTOP-O208I2J>
2019-07-22 12:17:32 +0200

    dilluns 22

--- Exercicis/Exercicis/src/PAC6/Stats.java
+++ Exercicis/Exercicis/src/PAC6/Stats.java
@@ -1,21 +1,23 @@
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         NumberStatistics stats = new NumberStatistics();
+        NumberStatistics stats2 = new NumberStatistics();
+        NumberStatistics stats3 = new NumberStatistics();
         System.out.println("Type numbers: ");
-        
+
         while (true) {
-                 
-        int number = Integer.parseInt(reader.nextLine());
-        stats.addNumber(number);
-        if (number == - 1) {
-            System.out.println(stats.sum());
-            break;
-           }
+
+            int number = Integer.parseInt(reader.nextLine());
+
+            if (number != - 1) {
+                stats.addNumber(number);
+
+            } else {
+                System.out.println(stats.sum());
+                System.out.println(stats.average());
+            }
+        }
     }
-}
 }
